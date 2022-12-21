0ee87501a36649c4cd03836b9f8fa7eb45ca6946
lenbo <allenbora@gmail.com>
2022-12-22 00:49:09 +0800

    twelfth

---
+++ ModulesFunction.py
@@ -0,0 +1,12 @@
+#math module Functions..........................................
+
+import math
+
+sqrtValue = math.pow(25.3, 2)
+print("pi:", math.pi)
+print("ceil:", math.ceil(7.24))
+print("floor: ", math.floor(7.24))
+print("sqrt:", math.sqrt(4.5))
+print("sin", math.sin(80))
+print("cos", math.cos(90))
+print("tan", math.tan(95))
